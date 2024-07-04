4ad075ae87f1245b460d80c7aa551a3ebbd4a9cd
jobran28 <142024805+jobran28@users.noreply.github.com>
2024-07-05 00:24:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)

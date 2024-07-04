10a081b7bd7f5318dd6c009b1f5c2f9cd4ead77c
jobran28 <142024805+jobran28@users.noreply.github.com>
2024-07-05 00:21:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from typing import Optional
-
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
@@ -12,3 +11,6 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)

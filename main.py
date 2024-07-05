d5b03a07144783a368734c3f0ad1f46861357d5b
jobran28 <142024805+jobran28@users.noreply.github.com>
2024-07-05 09:50:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"hi"}
+    return {"hi ,polina server is working"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):

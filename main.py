7d53a8e490b77a7e96f507d19769fe0317dd64fc
jobran28 <142024805+jobran28@users.noreply.github.com>
2024-07-05 09:53:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ app = FastAPI()
 async def root():
     return {"hi ,polina server is working"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0")
